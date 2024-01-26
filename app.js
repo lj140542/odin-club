86eb3dbdffc181045ac22592657da0490ff50163
lj140542 <jonathan.lefebvre76600@gmail.com>
2024-01-26 19:16:07 +0100

    Updates the index view and style

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser({ secret: process.env.SECRET }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
